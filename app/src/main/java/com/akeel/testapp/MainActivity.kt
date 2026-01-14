dae69a712c766686502ebd4aed7e2e4574f1e164
Akeel shafi <akeelshafi20@gmail.com>
2026-01-14 13:00:12 +0530

    Day 14 commit

--- app/src/main/java/com/akeel/testapp/MainActivity.kt
+++ app/src/main/java/com/akeel/testapp/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("LIFECYCLE","onCreate called")
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
