20c2ca41f6213fed0006321214ceee03965f655b
Akeel shafi <akeelshafi20@gmail.com>
2026-01-15 07:25:12 +0530

    Day 15 commit

--- app/src/main/java/com/akeel/testapp/MainActivity.kt
+++ app/src/main/java/com/akeel/testapp/MainActivity.kt
@@ -21,10 +21,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    override fun onResume() {
-        super.onResume()
-        Log.d("LifeCycle", "onResume called")
-    }
+
 
     override fun onPause() {
         super.onPause()
