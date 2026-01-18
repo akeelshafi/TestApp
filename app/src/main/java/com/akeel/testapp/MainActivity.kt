3185bf3c95f5d69741434c9be8b193655f3db1c6
Akeel shafi <akeelshafi20@gmail.com>
2026-01-18 11:53:13 +0530

    Day 18 commit

--- app/src/main/java/com/akeel/testapp/MainActivity.kt
+++ app/src/main/java/com/akeel/testapp/MainActivity.kt
@@ -22,10 +22,6 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    override fun onStop() {
-        super.onStop()
-        Log.d("LIFECYCLE", "onStop called")
-    }
 
     override fun onRestart() {
         super.onRestart()
