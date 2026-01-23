c6cfb6dd5104c840c62c57d65123983d0e9ae5e0
Akeel shafi <akeelshafi20@gmail.com>
2026-01-23 11:52:05 +0530

    Day 23 commit

--- app/src/main/java/com/akeel/testapp/MainActivity.kt
+++ app/src/main/java/com/akeel/testapp/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
 
         textView.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra("key", "Akeel shafi kana")
             startActivity(intent)
 
         }
--- app/src/main/java/com/akeel/testapp/MainActivity2.kt
+++ app/src/main/java/com/akeel/testapp/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.akeel.testapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,17 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
+
+        val textView = findViewById<TextView>(R.id.textView2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val value = intent.getStringExtra("key")
+        textView.text = value
+
+
     }
 }
